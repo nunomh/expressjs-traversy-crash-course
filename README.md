47f89ae96a7ab5acdfeb85d3d9e72d6227c9fe52
Nuno Henriques <nuno.henriques@izertislabs.pt>
2024-10-23 12:11:10 +0100

    Static folder and static pages. Start creating get all posts and get single post

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ Based on the youtube video https://www.youtube.com/watch?v=CnH3kAXSrmU
 ## Technologies
 
 - Express.js
+
+## Install and start
+
+Install: `npm install`
+Start dev: `npm run dev` (already includes --watch for auto-reload)
+
+## Folder Structure
+
+- public : static files
+
+## .env
+
+- PORT
--- server.js
+++ server.js
@@ -1,10 +1,44 @@
 const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8000;
 
 const app = express();
 
-app.get('/', (req, res) =>
+// ----------- STATIC FOLDER AND STATIC FILES SETUP
+
+// setup static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// although the code below is commented, the '/'  route is still being handled by express.static, opening the index.html
+// app.get('/', (req, res) =>
+// {
+//     // res.sendFile(path.join(__dirname, 'public', 'index.html')); // alternative way to serve index.html
+//     res.sendFile('index.html'); // using app.use(express.static(...));
+// });
+
+// the code below is commented, but  it still handles the route because about.html exists  in the public folder, to access it it will need the .html (/about.html)
+// app.get('/about', (req, res) =>
+// {
+//     res.sendFile(path.join(__dirname, 'public', 'about.html'));
+// });
+
+let posts = [
+    { id: 1, title: 'post 1' },
+    { id: 2, title: 'post 2' },
+    { id: 3, title: 'post 3' }
+];
+
+// get all posts
+app.get('/api/posts', (req, res) =>
 {
-    res.send('Hello World!');
+    res.json(posts);
 });
 
-app.listen(8000, () => console.log(`Server is running on port 8000`));
+// get single post
+app.get('/api/posts/:id', (req, res) =>
+{
+    const id = parseInt(req.params.id);
+    res.json(posts.filter((post) => post.id === id));
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
